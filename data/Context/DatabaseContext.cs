00844bca0fca57044cb5303d46d1ef1e88c839af
LuisLenzi <luisgustavolenzi@hotmail.com>
2021-11-23 08:59:16 -0300

    Adicionar arquivos de projeto.

---
+++ data/Context/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using AppMvcBasica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace data.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string)))) 
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DatabaseContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
